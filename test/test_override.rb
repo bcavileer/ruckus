59712ba20c4573e4fd35aabdc9252b716115ae56
tduehr <td@matasano.com>
2010-04-15 10:06:54 -0500

    fix test_override namespace

--- test/test_override.rb
+++ test/test_override.rb
@@ -1,17 +1,17 @@
 require 'ruckus'
 require 'test/unit'
 
-class TestOverridden < Ruckus::Structure
-  n32 :primary, :value => 1
-  n32 :secondary, :value => 2
-end
-
-class TestOverrider < TestOverridden
-  override :primary, 2
-  override :secondary, 1
-end
-
 class TestOverride < Test::Unit::TestCase
+  class TestOverridden < Ruckus::Structure
+    n32 :primary, :value => 1
+    n32 :secondary, :value => 2
+  end
+
+  class TestOverrider < TestOverridden
+    override :primary, 2
+    override :secondary, 1
+  end
+
   def test_override
     overridden = TestOverridden.new
     overrider = TestOverrider.new
