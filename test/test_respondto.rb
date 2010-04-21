a961fea1b41c96a8c2475c868fc1a744b40c9a59
tduehr <td@matasano.com>
2010-04-21 08:11:44 -0500

    add test cases for respond_to?

---
+++ test/test_respondto.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require 'ruckus'
+
+class TestRespondTo < Test::Unit::TestCase
+  class TestRespondClass < Ruckus::Structure
+    n32 :test_element, :value => 1
+    tag_bit :tag?, :value => 1
+    n32 :tagged_element, :value =>2, :tag => :tagged
+  end
+  
+  def test_respond_to?
+    trc = TestRespondClass.new
+    assert(trc.respond_to?(:test_element))
+    assert(trc.respond_to?(:tag?))
+    assert(trc.respond_to?(:tagged_element))
+    assert(trc.respond_to?(:tagged))
+  end
+end
