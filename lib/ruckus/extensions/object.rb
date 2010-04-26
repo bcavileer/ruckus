8eb071ba2153335ed8abe7bfa1cad923d657a748
tduehr <td@matasano.com>
2010-04-26 08:41:56 -0500

    added tap to extentions for < 1.9 rubies

--- lib/ruckus/extensions/object.rb
+++ lib/ruckus/extensions/object.rb
@@ -1,22 +1,27 @@
 class Object
-    module ObjectExtensions
-        # Every object has a "singleton" class, which you can think
-        # of as the class (ie, 1.metaclass =~ Fixnum) --- but that you
-        # can modify and extend without fucking up the actual class.
+  module ObjectExtensions
+    # Every object has a "singleton" class, which you can think
+    # of as the class (ie, 1.metaclass =~ Fixnum) --- but that you
+    # can modify and extend without fucking up the actual class.
 
-        def metaclass; class << self; self; end; end
-        def meta_eval(&blk) metaclass.instance_eval &blk; end
-        def meta_def(name, &blk) meta_eval { define_method name, &blk }; end
-        def try(meth, *args); send(meth, *args) if respond_to? meth; end
+    def metaclass; class << self; self; end; end
+    def meta_eval(&blk) metaclass.instance_eval &blk; end
+    def meta_def(name, &blk) meta_eval { define_method name, &blk }; end
+    def try(meth, *args); send(meth, *args) if respond_to? meth; end
 
-        def through(meth, *args)
-            if respond_to? meth
-                send(meth, *args)
-            else
-                self
-            end
-        end
+    def through(meth, *args)
+      if respond_to? meth
+        send(meth, *args)
+      else
+        self
+      end
     end
-    include ObjectExtensions
+
+    def tap(&block)
+      yield self
+      self
+    end if !(Object.instance_methods.include? :tap)
+  end
+  include ObjectExtensions
 end
 
